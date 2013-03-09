588cd9d07535756a3542a065c3458e07aa8fd5a7
Ville Tuulos <team+foobar@bitdeli.com>
2013-03-09 02:14:26 +0000

    test segments

--- insight.py
+++ insight.py
@@ -1,14 +1,12 @@
 from bitdeli.insight import insight
 from bitdeli.widgets import Text, Bar, Table
-from discodb.query import Q, Literal, Clause
 
 @insight
 def view(model, params):
-    params = {'events': ['$signup', 'Clip created']}
-    def steps(events):
-        for i in range(len(events)):
-            q = Q([Clause([Literal(event)]) for event in events[:i + 1]])
-            yield events[i], len(model.query(q))
-    return [Bar(size=(12, 6),
-                data=list(steps(params['events'])))]
+    if type(model) == tuple:
+        yield Text(size=(12, 2),
+                   data={'head': 'yay %s' % model})
+    else:
+        yield Text(size=(12, 2),
+                   data={'head': 'Add a segment to get started'})
     
