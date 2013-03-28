6d24cd5aca206a5f5cd25b7bf1a5f47875204a48
Ville Tuulos <ville@bitdeli.com>
2013-03-28 03:47:43 +0000

    fix model for jsapi

--- jsapi/model.py
+++ jsapi/model.py
@@ -1,16 +1,8 @@
 from bitdeli.model import model, segment_model
-from collections import namedtuple
+from collections import namedtuple, Counter
 
 MAX_LEN = 32
 CUTOFF = 4
-
-def count_events(hours):
-    s = 0
-    for hour, count in hours:
-        s += count
-        if s >= CUTOFF:
-            break
-    return 'l' if s < CUTOFF else 'h'
 
 @model
 def build(profiles):
@@ -18,13 +10,20 @@ def build(profiles):
         uid = profile.uid
         if not uid:
             continue
-        for event, hours in profile['events'].iteritems():
-            event = event.encode('utf-8')
-            yield 'e:%s%s' % (count_events(hours), event), uid
-        for prop_name, prop_values in profile['properties'].iteritems():
-            prop_name = prop_name.encode('utf-8')
-            for v in frozenset(prop_value[:MAX_LEN] for prop_value in prop_values):
-                yield 'p%s:%s' % (prop_name, v.encode('utf-8')), uid
+        eventcount = Counter()
+        propset = set()
+        for event in profile['events']:
+            event = event[3]
+            eventcount[event['$event_name'].encode('utf-8')] += 1
+            for prop_name, prop_value in event.iteritems():
+                if prop_name[0] != '$':
+                    propset.add('p%s:%s' % (prop_name.encode('utf-8'),
+                                            str(prop_value)[:MAX_LEN].encode('utf-8')))
+        for event, count in eventcount.iteritems():
+            prefix = 'l' if count < CUTOFF else 'h'
+            yield 'e:%s%s' % (prefix, event), uid
+        for prop in propset:
+            yield prop, uid     
 
 @segment_model
 def segment(model, segments, labels):
